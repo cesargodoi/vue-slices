2860238dc4d2cbf13e3fc87bb56c1e2f3ed4737f
cesargodoi <cesar.godoi@gmail.com>
2021-01-19 08:43:43 -0300

    SingleSlice component

---
+++ src/components/slices/SingleSlice.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>{{ slice.slice }}</h2>
+  <p>{{ slice.description }}</p>
+  <ul>
+    <li v-for="example in slice.examples" :key="example">{{ example }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "TheSlices",
+  props: { slice: Object },
+};
+</script>
--- src/components/slices/TheSlices.vue
+++ src/components/slices/TheSlices.vue
@@ -1,9 +1,38 @@
 <template>
   <h1>The Slices</h1>
+  <ul>
+    <single-slice v-for="slice in slices" :key="slice.id" :slice="slice" />
+  </ul>
 </template>
 
 <script>
+import SingleSlice from "./SingleSlice.vue";
 export default {
+  components: { SingleSlice },
   name: "TheSlices",
+  data() {
+    return {
+      slices: [
+        {
+          id: 1,
+          slice: "$emit",
+          description:
+            "Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá.",
+          examples: ["https://vuejs.org/", "https://stackoverflow.com/"],
+        },
+        {
+          id: 2,
+          slice: "v-for",
+          description:
+            "Manduma pindureta quium dia nois paga. In elementis mé pra quem é amistosis quis leo.",
+          examples: [
+            "https://vuejs.org/",
+            "https://stackoverflow.com/",
+            "https://google.com",
+          ],
+        },
+      ],
+    };
+  },
 };
 </script>
